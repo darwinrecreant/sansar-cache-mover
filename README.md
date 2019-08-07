57d60133d239bbdfa9a39b1b33f4a7477997c476
EvoAv <darwinrecreant@gmail.com>
2019-08-07 21:48:47 +0000

    clarification

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This windows script will set a new location to store Sansar cache that is normal
 3. Double click `sansar-cache-mover.bat`
 4. Follow the instructions and paste the path of the new folder when it asks you.
 
-The script will delete your existing sansar cache. If you do not want it to be deleted, then copy the contents of the old cache folder to your new folder before running this script.
+The script will delete your existing sansar cache folder, which includes your Sansar preferences. If you do not want it to be deleted, then copy the contents of the old cache folder to your new folder before running this script.
 
 If you want to use the old folder location again, go back to AppData\Local and delete `LindenLab` folder link (it will have a shortuct icon on it). Next time Sansar is opened it will create a new cache folder in the old place.
 
--- sansar-cache-mover.bat
+++ sansar-cache-mover.bat
@@ -1,5 +1,7 @@
 @echo OFF
 
+rem Check for valid app data location
+
 if not exist "%USERPROFILE%\AppData\Local" (
   echo AppData not found.
   pause 10
@@ -13,6 +15,7 @@ echo App data found, now will delete Sansar cache (%LL%), continue? ([y]/n)
 set /p continue2=
 if /i "%continue2%" == "n" goto END
 
+rem If LindenLab folder already exists, delete it. If it's a symlink then just delete the link.
 if exist "%LL%" (
   rmdir %LL% /S
 )
@@ -23,13 +26,15 @@ echo.
 echo What path do you want to store the Sansar cache folder in? (someting like D:\AppData\Local\LindenLab)
 set /p newLL=
 
+rem If new path does not have :\ in it then ask for path again.
+
 If NOT "%newLL%"=="%newLL::\=%" (
   if "%newLL%" == "" (
     echo This must be an path like D:\AppData\Local\LindenLab, or any folder
     goto ASKPATH
   )
 ) else (
-  echo This must be an path like D:\AppData
+  echo This must be an path like D:\AppData\Local\LindenLab, or any folder
   goto ASKPATH
 )
 
@@ -38,10 +43,16 @@ echo Creating symbolic link between old Sansar folder to new Sansar folder (%new
 set /p continue3=
 if /i "%continue3%" == "n" goto END
 
+rem Create folder of new path if it does not exist
 mkdir %newLL%
+
+rem Create the symlink
 mklink /d %LL% %newLL%
+
+rem Done
 
 echo.
 echo Sansar cache stored in new location successfully. From now on to delete cache, delete it in the new location.
 pause 10
+
 :END
